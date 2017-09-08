5e5f7ced94ad6a73504ba94540a0a3e9cf3b878a
abhineet27 <abhineet.sitcsc@gmail.com>
2017-09-08 14:03:30 +0530

    added config for auth layer

--- server/src/main/java/com/plivo/smsgateway/server/Application.java
+++ server/src/main/java/com/plivo/smsgateway/server/Application.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author abhineet
+ *
+ */
 @ComponentScan("com.plivo")
 @SpringBootApplication
 public class Application {
---
+++ server/src/main/java/com/plivo/smsgateway/server/config/AuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.plivo.smsgateway.server.config;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author abhineet
+ *
+ */
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+	@Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+      throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 403 - " + authEx.getMessage());
+    }
+	
+	@Override
+    public void afterPropertiesSet() throws Exception {
+        setRealmName("smsgateway");
+        super.afterPropertiesSet();
+    }
+}
